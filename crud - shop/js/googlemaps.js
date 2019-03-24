bc7b06ebe3f9560855b967e803204518965245c5
Shawn Phelan <phelo10@live.co.uk>
2019-03-24 22:22:16 +0000

    sprint 5
    sprint 5

--- crud - shop/js/googlemaps.js
+++
@@ -1,7 +0,0 @@
-function loadMap(){
-    
-     map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 52.2461, lng: 7.1252},
-          zoom: 8
-        });
-}
--- crud - shop/user/footer.php
+++
@@ -1,9 +0,0 @@
-<!--The footer of the page-->
-<footer style="background-color: #333333; height: 80px; margin-top: 50px; margin-bottom:0;">
-    <div class="container">
-        <div class="row text-center" style="padding-top: 30px;">
-            <p style="color: white !important">Copyright &copy; Waterford FC Prototype 2019. Not a representation of Waterford FC</p>
-        </div>
-    </div>
-    
-</footer>
